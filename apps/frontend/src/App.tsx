69632061ce0029ea0a93e4537648a80fdfa32429
AdamPoi <isadamrr@gmail.com>
2024-08-14 08:15:56 +0700

    feat: add react router

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,14 @@
-function App() {
-  return (
-    <>
-      <div className="bg-red-500">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-    </>
-  );
-}
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Dashboard';
+
+const App: React.FC = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+  </Routes>
+);
 
 export default App;
---
+++ apps/frontend/src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => (
+  <div>
+    <h1>This is the Login Page</h1>
+  </div>
+);
+
+export default Login;
